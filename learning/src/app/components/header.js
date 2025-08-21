5a726e5fc67f5531f6ab306805b0948e9796a8c4
abhishekkumar0123 <abhishekkumar0123@gmail.com>
2025-08-21 17:15:42 +0530

    logo

--- learning/src/app/components/header.js
+++ learning/src/app/components/header.js
@@ -1,22 +1,34 @@
-import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import React from 'react';
+import '../../../public/sass/pages/demo.scss';
+import { Col, Container, Row } from 'react-bootstrap';
 
+import Image from 'next/image';
 const Header = () => {
   return (
     <>
-        <section className='header_section'>
-            <Container>
-            <Row>
-                <Col xxl={12} xl={12} lg={12} md={12} sm={12} xs={12}>
-                <div className="inner_area">
-                    <h3>Header section ! </h3>
+      <section className="header_section">
+        <Container>
+          <Row>
+            <Col xxl={12} xl={12} lg={12} md={12} sm={12} xs={12}>
+              <div className="inner_area">
+                <div className="left_area">
+                  <div className="image_area">
+                    <Image
+                      src="/images/logo.png"
+                      alt="Hero Image"
+                      width={120}
+                      height={80}
+                    />
+                  </div>
                 </div>
-                </Col>
-            </Row>
-            </Container>
-        </section>
+                <div className="right_area"></div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- learning/src/app/homepage/page.js
+++ learning/src/app/homepage/page.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import "../../../public/sass/pages/demo.scss"
+import '../../../public/sass/pages/demo.scss';
 import Blog from '../components/blog';
 import { Col, Container, Row } from 'react-bootstrap';
 
 const Homepage = () => {
   return (
     <>
-      <section className='banner_section'>
+      <section className="banner_section">
         <Container>
           <Row>
             <Col xxl={12} xl={12} lg={12} md={12} sm={12} xs={12}>
-            <div className="inner_area">
-              <h3>Banner section</h3>
-            </div>
+              <div className="inner_area"></div>
             </Col>
           </Row>
         </Container>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
