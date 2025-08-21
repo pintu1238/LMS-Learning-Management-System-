dbcc9a425a14da3126cbffdc6a8fb194fee2b492
Vicky <vickygup9900@gmail.com>
2025-08-21 15:37:55 +0530

    setup

---
+++ learning/src/app/components/header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+const Header = () => {
+  return (
+    <>
+        <section className='header_section'>
+            <Container>
+            <Row>
+                <Col xxl={12} xl={12} lg={12} md={12} sm={12} xs={12}>
+                <div className="inner_area">
+                    <h3>Header section</h3>
+                </div>
+                </Col>
+            </Row>
+            </Container>
+        </section>
+    </>
+  )
+}
+
+export default Header
---
+++ learning/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Homepage from "./homepage/page";
+
+export default function Home() {
+  return (
+    <>
+    <Homepage />
+    </>
+  );
+}
