4530bc505f5aa6868ca9a9c132510a960f98cca1
Vicky <vickygup9900@gmail.com>
2025-08-23 10:13:35 +0530

    swiper

--- learning/src/app/components/third_sec.js
+++ learning/src/app/components/third_sec.js
@@ -7,9 +7,14 @@ import Card3_1 from "../../../public/images/third_sec1.webp";
 import Card3_2 from "../../../public/images/third_sec2.webp";
 import Card3_3 from "../../../public/images/third_sec3.webp";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faStar);
+
 import { Swiper, SwiperSlide } from "swiper/react";
 
-import { Navigation, Pagination, Scrollbar} from "swiper/modules";
+import { Autoplay, Navigation, Pagination, Scrollbar } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/navigation";
@@ -28,99 +33,144 @@ const Third_sec = () => {
               Career Accelerators.
             </p>
           </div>
-          <Row>
-            <Swiper
-              modules={[Navigation, Pagination, Scrollbar]}
-              spaceBetween={50}
-              slidesPerView={3}
-              onSlideChange={() => console.log("slide change")}
-              onSwiper={(swiper) => console.log(swiper)}
-              navigation
-              pagination={{ clickable: true }}
-              // scrollbar={{ draggable: true }}
-            >
-              <SwiperSlide>
-                <div className="career_card">
-                  <div className="card_image">
-                    <Image
-                      src={Card3_1}
-                      alt="Full Stack Web Developer"
-                      title="images"
-                      fetchPriority="low"
-                    />
-                  </div>
-                  <div className="card_content">
-                    <h4>Full Stack Web Developer</h4>
-                    <div className="rating_row">
-                      <div className="star_logo badge_item">
-                        <FontAwesomeIcon
-                          icon={["fas", "star"]}
-                          className="star_icon"
-                        />
-                        4.7
-                      </div>
-                      <p className="badge_item">455K ratings</p>
-                      <p className="badge_item">87.8 total hours</p>
-                    </div>
-                  </div>
-                </div>
-              </SwiperSlide>
+          <div className="swiper-controls">
+            <div className="swiper-button-prev"></div>
+            <div className="swiper-button-next"></div>
+          </div>
 
-              <SwiperSlide>
-                <div className="career_card">
-                  <div className="card_image">
-                    <Image
-                      src={Card3_2}
-                      alt="Full Stack Web Developer"
-                      title="images"
-                      fetchPriority="low"
-                    />
-                  </div>
-                  <div className="card_content">
-                    <h4>Data Science</h4>
-                    <div className="rating_row">
-                      <div className="star_logo badge_item">
-                        <FontAwesomeIcon
-                          icon={["fas", "star"]}
-                          className="star_icon"
-                        />
-                        4.9
-                      </div>
-                      <p className="badge_item">200K ratings</p>
-                      <p className="badge_item">30.8 total hours</p>
-                    </div>
-                  </div>
-                </div>
-              </SwiperSlide>
+          <Swiper
+            modules={[Navigation, Pagination, Scrollbar, Autoplay]}
+            spaceBetween={50}
+            slidesPerView={3}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            loop={true}
+            navigation={{
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            }}
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            // onSwiper={(swiper) => console.log(swiper)}
+            // onSlideChange={() => console.log("slide change")}
+            breakpoints={{
+              // Smaller phones
+              320: { slidesPerView: 1, spaceBetween: 10 },
 
-              <SwiperSlide>
-                <div className="career_card">
-                  <div className="card_image">
-                    <Image
-                      src={Card3_1}
-                      alt="Full Stack Web Developer"
-                      title="images"
-                      fetchPriority="low"
-                    />
-                  </div>
-                  <div className="card_content">
-                    <h4>Full Stack Web Developer</h4>
-                    <div className="rating_row">
-                      <div className="star_logo badge_item">
-                        <FontAwesomeIcon
-                          icon={["fas", "star"]}
-                          className="star_icon"
-                        />
-                        4.7
-                      </div>
-                      <p className="badge_item">455K ratings</p>
-                      <p className="badge_item">87.8 total hours</p>
+              // 361px - 575px
+              361: { slidesPerView: 1, spaceBetween: 15 },
+
+              // 576px - 767px
+              576: { slidesPerView: 2, spaceBetween: 20 },
+
+              // 768px - 991px (tablets)
+              768: { slidesPerView: 2.5, spaceBetween: 20 },
+
+              // 992px - 1199px (medium desktops)
+              992: { slidesPerView: 3, spaceBetween: 25 },
+
+              // 1200px - 1299px
+              1200: { slidesPerView: 3, spaceBetween: 25 },
+
+              // 1300px - 1399px
+              1300: { slidesPerView: 3, spaceBetween: 30 },
+
+              // 1400px - 1599px
+              1400: { slidesPerView: 3, spaceBetween: 30 },
+
+              // 1600px - 1679px
+              1600: { slidesPerView: 5, spaceBetween: 35 },
+
+              // 1680px - 1919px
+              1680: { slidesPerView: 5.5, spaceBetween: 35 },
+
+              // 1920px and above
+              1920: { slidesPerView: 6, spaceBetween: 40 },
+            }}
+          >
+            <SwiperSlide>
+              <div className="career_card">
+                <div className="card_image">
+                  <Image
+                    src={Card3_1}
+                    alt="Full Stack Web Developer"
+                    title="images"
+                    fetchPriority="low"
+                  />
+                </div>
+                <div className="card_content">
+                  <h4>Full Stack Web Developer</h4>
+                  <div className="rating_row">
+                    <div className="star_logo badge_item">
+                      <FontAwesomeIcon
+                        icon={["fas", "star"]}
+                        className="star_icon"
+                      />
+                      4.7
                     </div>
+                    <p className="badge_item">455K ratings</p>
+                    <p className="badge_item">87.8 total hours</p>
                   </div>
                 </div>
-              </SwiperSlide>
-            </Swiper>
-          </Row>
+              </div>
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <div className="career_card">
+                <div className="card_image">
+                  <Image
+                    src={Card3_2}
+                    alt="Full Stack Web Developer"
+                    title="images"
+                    fetchPriority="low"
+                  />
+                </div>
+                <div className="card_content">
+                  <h4>Data Science</h4>
+                  <div className="rating_row">
+                    <div className="star_logo badge_item">
+                      <FontAwesomeIcon
+                        icon={["fas", "star"]}
+                        className="star_icon"
+                      />
+                      4.9
+                    </div>
+                    <p className="badge_item">200K ratings</p>
+                    <p className="badge_item">30.8 total hours</p>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <div className="career_card">
+                <div className="card_image">
+                  <Image
+                    src={Card3_1}
+                    alt="Full Stack Web Developer"
+                    title="images"
+                    fetchPriority="low"
+                  />
+                </div>
+                <div className="card_content">
+                  <h4>Full Stack Web Developer</h4>
+                  <div className="rating_row">
+                    <div className="star_logo badge_item">
+                      <FontAwesomeIcon
+                        icon={["fas", "star"]}
+                        className="star_icon"
+                      />
+                      4.7
+                    </div>
+                    <p className="badge_item">455K ratings</p>
+                    <p className="badge_item">87.8 total hours</p>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
 
           {/* <Col xxl={4} xl={4} lg={4} md={6} sm={6} xs={12}>
             <div className="career_card">
