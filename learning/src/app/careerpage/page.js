b9cb777844502793de7a7ceaeb924ca05139405b
abhishekkumar0123 <abhishekkumar0123@gmail.com>
2025-09-04 12:50:13 +0530

    push

--- learning/src/app/careerpage/page.js
+++ learning/src/app/careerpage/page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../../public/sass/pages/demo.scss';
-import Blog from '../components/blog';
-import { Col, Container, Row } from 'react-bootstrap';
+// import Blog from '../components/blog';
+// import { Col, Container, Row } from 'react-bootstrap';
 import Career_sectiontwo from '../components/career_sectiontwo';
 import Career_banner from '../components/career_banner';
 
