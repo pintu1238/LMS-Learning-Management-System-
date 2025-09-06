4b52575889d6382dd9e0e21c13ce7ca1c722fcd8
Nitin Kumar <112533120+nitinkumar9877@users.noreply.github.com>
2025-09-06 15:26:02 +0530

    today work

--- learning/src/app/blogs/page.js
+++ learning/src/app/blogs/page.js
@@ -4,72 +4,99 @@ import { Col, Container, Row } from 'react-bootstrap'
 import bannerImg from "../../../public/images/blogs_banner.png";
 import "../../../public/sass/pages/blogs.scss";
 import Link from 'next/link';
+import DevPerson1 from "../../../public/images/dev_team_1.webp";
+import NavBlog from '../components/nav_blogs';
+import BannerBlogs from '../components/banner_blogs';
+import DevPageBlog from '../components/dev_blogs';
+import ItpageBlog from '../components/it_and_dev';
 const Blogs = () => {
     return (
         <>
-            <section className="blogs">
+            <BannerBlogs />
+            <NavBlog />
+            <DevPageBlog />
+            <ItpageBlog/>
+            <section className="develop">
                 <Container>
                     <Row>
                         <Col xxl={12} xl={12} lg={12} md={12} ms={12} xs={12}>
-                            <div className="inner_blogs">
-                                <div className="left">
-                                    <div className="contant">
-                                        <h1>Where possibilities begin</h1>
-                                        <p className="contant_heading">
-                                            We're a leading marketplace platform for learning and teaching online. Explore some of our most popular content and learn something new.
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className="right">
-                                    <div className="banner_img">
-                                        <Image src={bannerImg} alt='banner_img' title='banner_img' fetchPriority='low' height={300} width={400} />
-                                    </div>
-                                </div>
+                            <div className="inner_develop">
+                                <h3 className="aritcla_dev">Development Articles</h3>
+                                <Row>
+                                    <Col xxl={3} xl={3} lg={4} md={4} ms={6} xs={12}>
+                                        <div className="card_area">
+                                            {/* <p className="top_head">Development</p> */}
+                                            <h3 className="heading">Job Searching with AI: The Ultimate Guide with Example Prompts</h3>
+                                            <div className="lower">
+                                                <div className="left">
+                                                    <div className="person">
+                                                        <Image src={DevPerson1} alt="dev team" title='dev team' fetchPriority='low' height={30} width={30} />
+                                                    </div>
+                                                </div>
+                                                <div className="right">
+                                                    <div className="contant">
+                                                        <p>Genefa Murphy</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                    <Col xxl={3} xl={3} lg={4} md={4} ms={6} xs={12}>
+                                        <div className="card_area">
+                                            {/* <p className="top_head">Development</p> */}
+                                            <h3 className="heading">How to Become an AI Engineer</h3>
+                                            <div className="lower">
+                                                <div className="left">
+                                                    <div className="person">
+                                                        <Image src={DevPerson1} alt="dev team" title='dev team' fetchPriority='low' height={30} width={30} />
+                                                    </div>
+                                                </div>
+                                                <div className="right">
+                                                    <div className="contant">
+                                                        <p>Nedko Krastev</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                    <Col xxl={3} xl={3} lg={4} md={4} ms={6} xs={12}>
+                                        <div className="card_area">
+                                            {/* <p className="top_head">Development</p> */}
+                                            <h3 className="heading">How Learners Are Future-Proofing Their Careers With AI and Cloud</h3>
+                                            <div className="lower">
+                                                <div className="left">
+                                                    <div className="person">
+                                                        <Image src={DevPerson1} alt="dev team" title='dev team' fetchPriority='low' height={30} width={30} />
+                                                    </div>
+                                                </div>
+                                                <div className="right">
+                                                    <div className="contant">
+                                                        <p>James Young</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                    <Col xxl={3} xl={3} lg={4} md={4} ms={6} xs={12}>
+                                        <div className="card_area">
+                                            {/* <p className="top_head">Development</p> */}
+                                            <h3 className="heading">What Is Vibe Coding?</h3>
+                                            <div className="lower">
+                                                <div className="left">
+                                                    <div className="person">
+                                                        <Image src={DevPerson1} alt="dev team" title='dev team' fetchPriority='low' height={30} width={30} />
+                                                    </div>
+                                                </div>
+                                                <div className="right">
+                                                    <div className="contant">
+                                                        <p>Carlos del Rio</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                </Row>
                             </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </section>
-            <section className="nav_banner">
-                <Container>
-                    <Row>
-                        <Col>
-                            <din className="innernav">
-                                <nav>
-                                    <ul>
-                                        <li>
-                                            <Link href="#">Development</Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">IT & Software</Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">Data Science</Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">Soft Skills</Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">HR and L&D</Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">Business</Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">Office Productivity</Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">Marketing</Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">Design</Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">Career Accelerator</Link>
-                                        </li>
-                                    </ul>
-                                </nav>
-                            </din>
                         </Col>
                     </Row>
                 </Container>
